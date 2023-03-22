22e6e104c99938793c4ed6b6849972ac8fb30a5f
Michishi-Ikeda <miikeda2001@gmail.com>
2023-03-22 16:05:11 +0900

    PUTに変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
         
     }
 
@@ -25,5 +26,23 @@ class PostController extends Controller
    {
        return view('posts/create');
    }
-}
+   public function store(Post $post,PostRequest $request)
+   {
+       $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+   }
+   
+   public function edit(Post $post)
+   {
+       return view('posts/edit')->with(['post' => $post]);
+   }
+   
+   public function update(Post $post,PostRequest $request)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect ('/posts/' . $post->id);
+    }
+   }
 ?>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,20 +10,22 @@
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
-       <form action "/posts" method = "POST">
+       <form action="/posts" method = "POST">
            @csrf
            <div class = "title">
                <h2>title</h2>
-               <input type = "text" name = post[title] placeholder = "タイトル">
+               <input type = "text" name = post[title] placeholder = "タイトル" value = {{ old('post.title')}}>
+               <p class = "title_error" style = "color:red">{{ $errors->first('post.title')}}</p>
            </div>
            <div class = "body">
                <h2>Body</h2>
-               <textarea name = "post[body]" placeholder = "今日も一日お疲れさまでした"></textarea> 
+               <textarea name = "post[body]" placeholder = "今日も一日お疲れさまでした" value = {{ old('post.body')}}></textarea> 
+               <p class = "body_error" style = "color:red">{{ $errors->first('post.body')}} </p>
            </div>
-           <input type="submit" value = "store">
+           <input type="submit" value = "保存"/>
        </form>
-        <div class="footer">
-            <a href="/">戻る</a>
+        <div class="back">
+            [<a href="/">back</a>]
         </div>
     </body>
 </html>
